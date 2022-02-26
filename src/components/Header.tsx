11b083e33af251c5127f43f92be2c5a01383ba18
Rustam <rustam2004sadulaev@mail.ru>
2022-02-26 17:57:40 +0300

    lesson-12

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS__d-ag5ZGjKWWC-j0okLJAQaqKwiuhAZcRw&usqp=CAU'/>
+        </header>
+    );
+};
+
+export default Header;
+
