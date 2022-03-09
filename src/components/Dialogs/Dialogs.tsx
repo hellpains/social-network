b03bf3e484eb7b3eac01107ba1c0bbfa39247ef1
Рустам Садулаев <rustam2004sadulaev@mail.ru>
2022-03-09 12:24:27 +0300

    'lesson-21'

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,33 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div >
-            Dialogs
+        <div className={s.dialogs} >
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    hellpains
+                </div>
+                <div className={s.dialog}>
+                    Xalifat
+                </div>
+                <div className={s.dialog}>
+                    pretty
+                </div>
+                <div className={s.dialog}>
+                    Oland
+                </div>
+                <div className={s.dialog}>
+                    hkmv
+                </div>
+                <div className={s.dialog}>
+                    rosul
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,23 +3,24 @@ import {NavLink} from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
-    const setActive = (navData: any) => navData.isActive ? s.active : '';
+    const activeClassName = (navData: any) => navData.isActive ? s.active : '';
+
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink className={setActive} to={'/profile'}>Profile</NavLink>
+                <NavLink className={activeClassName} to={'/profile'}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink className={setActive} to={'/dialogs'}>Messages</NavLink>
+                <NavLink className={activeClassName} to={'/dialogs'}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink className={setActive} to={'/newss'}>News</NavLink>
+                <NavLink className={activeClassName} to={'/newss'}>News</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink className={setActive} to={'/music'}>Music</NavLink>
+                <NavLink className={activeClassName} to={'/music'}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink className={setActive} to={'/settings'}>Settings</NavLink>
+                <NavLink className={activeClassName} to={'/settings'}>Settings</NavLink>
             </div>
         </nav>
     );
