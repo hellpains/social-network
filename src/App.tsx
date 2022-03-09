d5b24855bb168db8e0720bb0a1afa8a4379a41d4
Рустам Садулаев <rustam2004sadulaev@mail.ru>
2022-03-09 10:20:16 +0300

    'lesson-19'

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,36 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Routes, Route} from "react-router-dom";
+import {Settings} from "./components/Setting/Settings";
+import {Music} from "./components/Music/Music";
+import {Newss} from "./components/News/News";
 
 const App = () => {
     return (
+
         <div className='app-wrapper'>
-            <Header />
-            <Navbar />
+            <Header/>
+            <Navbar/>
             <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile/>*/}
+                <Routes>
+                    <Route path={'/dialogs'} element={<Dialogs/>}/>
+                    <Route path={'/profile'} element={<Profile/>}/>
+                    <Route path={'/newss'} element={<Newss/>}/>
+                    <Route path={'/music'} element={<Music/>}/>
+                    <Route path={'/settings'} element={<Settings/>}/>
+                </Routes>
             </div>
 
         </div>
+
     );
+}
+
+
+
+const News = () => {
+  
 }
 export default App;
 
---
+++ src/components/News/News.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Newss.module.css'
+
+export const Newss = () => {
+    return (
+        <div >
+            News
+        </div>
+    );
+};
